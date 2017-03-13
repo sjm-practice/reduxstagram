8ae71ce5865a06178244d044c8aba914b6a14b7b
stevenjmarsh <sjm.atl@gmail.com>
2017-03-13 15:04:27 -0400

    adding tests for posts reducers

--- imports/state/reducers/tests/posts.jest.js
+++ imports/state/reducers/tests/posts.jest.js
@@ -3,12 +3,44 @@
 /* eslint-disable func-names, prefer-arrow-callback, no-unused-expressions */
 /* eslint-disable import/no-extraneous-dependencies */
 import posts from "../posts";
+import { increment } from "../../actions/actionCreators";
 
 describe("posts reducer", function () {
-  it.skip("should test every switch case", function () {
+  it("should increment likes when receiving an INCREMENT_LIKES action", function () {
+    const postsOldState = [
+      {
+        code: "abc123",
+        caption: "Nice Pic.",
+        likes: 0,
+        id: "123",
+        display_src: "www.this_picture.org",
+      },
+    ];
+
+    const incrementAction = increment(0);
+    const postsNewState = posts(postsOldState, incrementAction);
+
+    expect(postsOldState[0].likes).toEqual(0); // do not mutate old state
+    expect(postsNewState[0].likes).toEqual(1); // return new state with correct value
   });
 
-  it.skip("should test the default case", function () {
-    //
+  it("should return current state when receiving an unknown action", function () {
+    const postsOldState = [
+      {
+        code: "abc123",
+        caption: "Nice Pic.",
+        likes: 0,
+        id: "123",
+        display_src: "www.this_picture.org",
+      },
+    ];
+
+    const unknownAction = {
+      type: "UNKNOWN_ACTION",
+      index: 0,
+    };
+
+    const postsNewState = posts(postsOldState, unknownAction);
+    expect(postsNewState).toEqual(postsOldState);
   });
 });
