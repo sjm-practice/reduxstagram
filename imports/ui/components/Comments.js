c8d6c16ec7f41d8a988f71811f015731a013d1e0
stevenjmarsh <sjm.atl@gmail.com>
2017-03-13 16:32:29 -0400

    displaying comments, and new comment form

--- imports/ui/components/Comments.js
+++ imports/ui/components/Comments.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, {
+  Component,
+} from "react";
 
-const Comments = () => (
-  <div className="comment">
-    Comments
-  </div>
-);
+class Comments extends Component {
+  renderComment(comment, i) {
+    return (
+      <div className="comment" key={i}>
+        <strong>{comment.user}</strong>
+        {comment.text}
+        <button className="remove-comment">&times;</button>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="comments">
+        { this.props.postComments.map(this.renderComment)}
+        <form className="comment-form" ref="commentForm">
+          <input type="text" ref="author" placeholder="author" />
+          <input type="text" ref="comment" placeholder="comment" />
+          <input type="submit" hidden />
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Comments;
--- imports/ui/components/Single.js
+++ imports/ui/components/Single.js
@@ -15,13 +15,16 @@ class Single extends Component {
   props: IProps;
 
   render() {
-    const i = this.props.posts.findIndex(post => post.code === this.props.params.postId);
+    const { postId } = this.props.params;
+
+    const i = this.props.posts.findIndex(post => post.code === postId);
     const post = this.props.posts[i];
+    const postComments = this.props.comments[postId] || [];
 
     return (
       <div className="single-photo">
         <Photo index={i} post={post} {...this.props} />
-        <Comments />
+        <Comments postComments={postComments} />
       </div>
     );
   }
