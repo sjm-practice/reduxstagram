638c4a91280fb8878ff6f75b67dbf93ebc30523e
stevenjmarsh <sjm.atl@gmail.com>
2017-03-13 16:09:14 -0400

    displaying initial single component

--- imports/ui/components/Single.js
+++ imports/ui/components/Single.js
@@ -2,13 +2,24 @@
 import React, {
   Component,
 } from "react";
+import Photo from "./Photo";
+import type { IPosts } from "../../state/reducers/posts";
+
+type IProps = {
+  posts: IPosts,
+};
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Single extends Component {
+  props: IProps;
+
   render() {
+    const i = this.props.posts.findIndex(post => post.code === this.props.params.postId);
+    const post = this.props.posts[i];
+
     return (
       <div className="single-photo">
-        single photo
+        <Photo index={i} post={post} {...this.props} />
       </div>
     );
   }
