245570963cd0c6a559bdfaa7fdc0bda19de00b82
stevenjmarsh <sjm.atl@gmail.com>
2017-03-13 17:26:53 -0400

    capturing comment data on form, not yet adding to state though

--- imports/ui/components/Comments.js
+++ imports/ui/components/Comments.js
@@ -3,6 +3,15 @@ import React, {
 } from "react";
 
 class Comments extends Component {
+  handleSubmit(e) {
+    e.preventDefault();
+    const { postId } = this.props.params;
+    const author = this.refs.author.value;
+    const comment = this.refs.comment.value;
+    this.props.addComment(postId, author, comment);
+    this.refs.commentForm.reset();
+  }
+
   renderComment(comment, i) {
     return (
       <div className="comment" key={i}>
@@ -17,7 +26,11 @@ class Comments extends Component {
     return (
       <div className="comments">
         { this.props.postComments.map(this.renderComment)}
-        <form className="comment-form" ref="commentForm">
+        <form
+          className="comment-form"
+          ref="commentForm"
+          onSubmit={e => this.handleSubmit(e)}
+        >
           <input type="text" ref="author" placeholder="author" />
           <input type="text" ref="comment" placeholder="comment" />
           <input type="submit" hidden />
--- imports/ui/components/Single.js
+++ imports/ui/components/Single.js
@@ -24,7 +24,7 @@ class Single extends Component {
     return (
       <div className="single-photo">
         <Photo index={i} post={post} {...this.props} />
-        <Comments postComments={postComments} />
+        <Comments postComments={postComments} {...this.props} />
       </div>
     );
   }
